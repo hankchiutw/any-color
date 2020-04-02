77b4a4b28dd6943252b3473cfca7913b88ca3a9f
hank <hankchiu.tw@gmail.com>
2020-04-02 21:54:14 +0800

    Refine saturation-canvas

--- src/popup/components/saturation-canvas.tsx
+++ src/popup/components/saturation-canvas.tsx
@@ -28,35 +28,37 @@ export class SaturationCanvas extends React.Component {
 
   private initCanvas = (element: HTMLCanvasElement) => {
     this.project = new paper.Project(element);
-    this.pointer = new paper.Path.Circle({
-      center: this.project.view.center,
-      radius: 6,
-      strokeColor: 'white',
-    });
 
     const { width, height } = this.project.view.viewSize;
     this.width = width;
     this.height = height;
+
+    // located at top-right corner
+    this.pointer = new paper.Path.Circle({
+      center: [this.width, 0],
+      radius: 6,
+      strokeColor: 'white',
+    });
 
     this.project.view.onMouseDrag = this.updateColor;
     this.project.view.onMouseDown = this.updateColor;
   };
 
   private updateColor = (event: paper.MouseEvent) => {
-    const { color, setColor } = this.context;
+   this.context.setColor(this.getPointColor(event.point));
+  };
 
-    const { x, y } = event.point;
+  private getPointColor(point: paper.Point): HSVColor {
+    const { x, y } = point;
     const px = Math.max(0, Math.min(x, this.width));
     const py = Math.max(0, Math.min(y, this.height));
     this.pointer.position = new paper.Point(px, py);
 
-    setColor(
-      color.clone({
-        s: px / this.width,
-        v: 1 - py / this.height,
-      })
-    );
-  };
+    return this.context.color.clone({
+      s: px / this.width,
+      v: 1 - py / this.height,
+    });
+  }
 
   render() {
     return (
