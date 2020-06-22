724a6953887089f947e3a586a6fbb8a02a16a33c
hank <hankchiu.tw@gmail.com>
2020-06-22 21:39:27 +0800

    test: customize chrome mock

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jest-environment-jsdom-sixteen',
-  setupFiles: ['jest-canvas-mock', 'jest-webextension-mock'],
+  setupFiles: ['jest-canvas-mock', './__mocks__/chrome.ts'],
   transformIgnorePatterns: ['node_modules/(?!(lit-element|lit-html)/)'],
   moduleNameMapper: {
     '~(.*)': '<rootDir>/src/$1',
