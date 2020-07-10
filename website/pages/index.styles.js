3d29462e431320a8c0c8f75ae779a95595107050
hank <hankchiu.tw@gmail.com>
2020-07-10 22:06:48 +0800

    feat(website): add footer

--- website/pages/index.styles.js
+++ website/pages/index.styles.js
@@ -15,6 +15,31 @@ export default css`
     padding: 0 10%;
   }
 
+  .crxAnchor {
+    display: block;
+    height: 50px;
+    border-radius: 25px;
+    background-color: rgba(64, 110, 113, 0.7);
+    line-height: 50px;
+    color: white;
+    font-family: Roboto;
+    margin: 40px auto 0;
+    padding: 0 20px;
+    text-align: center;
+    font-weight: 300;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: center;
+    margin: 40px 0 20px;
+  }
+
+  .footer a[data-icon] {
+    color: rgba(64, 110, 113);
+    margin: 0 4px;
+  }
+
   @media screen and (min-width: 480px) {
     .fake-browser {
       padding: 0 15%;
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -19,6 +19,43 @@ export default function Home() {
         <div className="fake-browser">
           <FakeBrowser></FakeBrowser>
         </div>
+        <a
+          className="crxAnchor"
+          href="https://chrome.google.com/webstore/detail/any-color/cmehpadapglhhambdiafddpfjdngonba"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Chrome extension
+        </a>
+        <div className="footer">
+          ©&nbsp;
+          <a
+            href="https://hankchiu.tw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hankchiu.tw
+          </a>
+          &nbsp;2020.
+          <a
+            data-icon="github"
+            href="https://github.com/hankchiutw"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <a
+            data-icon="linkedin"
+            href="https://www.linkedin.com/in/hankchiutw"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          <a
+            data-icon="stackoverflow"
+            href="https://stackoverflow.com/users/6040102/hank-chiu"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+        </div>
       </div>
     </>
   );
