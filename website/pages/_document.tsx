d8803e2f94a83157684ac8ad9e8c2eacfe5563d8
hank <hankchiu.tw@gmail.com>
2020-07-17 09:42:47 +0800

    feat(website): add gtag and hotjar

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -12,6 +12,40 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-172921793-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (window.location.hostname === 'localhost') {
+                  window['ga-disable-UA-172921793-1'] = true;
+                }
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-172921793-1');
+              `,
+            }}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (window.location.hostname !== 'localhost') {
+                  (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:1905648,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                }
+              `,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
